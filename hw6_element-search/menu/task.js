b7aeb910f99638ac3c5997d0b069757251684a65
zhsaule <zhsaule@gmail.com>
2023-10-21 20:16:22 +0600

    Fix HW6 modify

--- hw6_element-search/menu/task.js
+++ hw6_element-search/menu/task.js
@@ -1,26 +1,32 @@
 // Получаем все пункты меню с классом .menu__link и регистрируем обработчик события для каждого
-const menuLinks = Array.from(document.querySelectorAll('.menu__link'));
-menuLinks.forEach((link) => {
-    link.onclick = () => {
-        // Находим ближайший родительский элемент .menu__item
-        const menuItem = link.closest('.menu__item'); 
-        if (menuItem) {
-            // Находим вложенное подменю
-            const submenu = menuItem.querySelector('.menu_sub'); 
-            if (submenu) {
-                // Закрываем другие открытые вложенные меню
-                const otherSubmenus = Array.from(document.querySelectorAll('.menu_active'));
-                otherSubmenus.forEach((otherSubmenu) => {
-                    if (otherSubmenu !== submenu) {
-                        otherSubmenu.classList.remove('menu_active');
-                    }
-                });
-                //Переключаем класс menu_active у вложенного меню
-                submenu.classList.toggle('menu_active');
+const menuLinks = document.querySelectorAll('.menu__link');
 
-            }
+Array.from(menuLinks).forEach((link) => {
+    link.onclick = () => {
+
+        // Находим вложенное подменю
+        const submenu = link.closest('.menu__item').querySelector('.menu_sub'); 
+        if (submenu) {
+            //Открываем вложенные подменю
+            submenu.classList.toggle('menu_active');
+           
+            // Закрываем другие открытые вложенные меню
+            closeOtherSubmenus(submenu);
+            
+            // Запрещаем переход по ссылке для вложенного подменю
+            return false;
+
         }
-        //Отключаем переход по ссылке
-        return false;
+
     }
 });
+
+// Функция для закрытия всех других открытых подменю
+function closeOtherSubmenus(openSubmenu) {
+    const allSubmenus = document.querySelectorAll('.menu_active');
+    Array.from(allSubmenus).forEach(submenu => {
+        if (submenu !== openSubmenu) {
+            submenu.classList.remove('menu_active');
+        }
+    });
+  }
--- hw6_element-search/slider/task.js
+++ hw6_element-search/slider/task.js
@@ -3,10 +3,11 @@ const arrowPrev = document.querySelector('.slider__arrow_prev');
 const arrowNext = document.querySelector('.slider__arrow_next');
 const dots = document.querySelectorAll('.slider__dot');
 
-let currentSlide = 0;
+// let currentSlide = 0;
 
-// Функция обновления слайдера
+ // Функция обновления слайдера
 const updateSlider = () => {
+    const currentSlide = Array.from(sliders).findIndex(item => item.classList.contains('slider__item_active'));
     sliders.forEach((item, index) => {
         item.classList.toggle('slider__item_active', index === currentSlide);
     });
@@ -18,14 +19,33 @@ const updateSlider = () => {
 
 // Функция перехода к предыдущему слайду
 const goToPrevSlide = () => {
-    currentSlide = (currentSlide - 1 + sliders.length) % sliders.length;
+    updateSlider();
+    const currentSlide = Array.from(sliders).findIndex(item => item.classList.contains('slider__item_active'));
+    const prevSlide = (currentSlide - 1 + sliders.length) % sliders.length;
+    closeOther(currentSlide);
+    sliders[prevSlide].classList.add('slider__item_active');    
     updateSlider();
 };
 
 // Функция перехода к следующему слайду
 const goToNextSlide = () => {
-    currentSlide = (currentSlide + 1) % sliders.length;
     updateSlider();
+    const currentSlide = Array.from(sliders).findIndex(item => item.classList.contains('slider__item_active'));
+    const nextSlide = (currentSlide + 1) % sliders.length;
+    closeOther(currentSlide);
+    sliders[nextSlide].classList.add('slider__item_active');    
+    updateSlider();
+};
+
+// Функция для деактивации всех других открытых слайдов
+const closeOther = (activeSlider) => {
+    const activeSliders = document.querySelectorAll('.slider__item_active');
+
+    Array.from(activeSliders).forEach(sItems => {
+        if (sItems !== activeSlider) {
+            sItems.classList.remove('slider__item_active');
+        }
+    });
 };
 
 // Обработчики событий
@@ -34,10 +54,12 @@ arrowNext.onclick = goToNextSlide;
 
 dots.forEach((dot, index) => {
     dot.onclick = () => {
-        currentSlide = index;
+        updateSlider();
+        closeOther(index);
+        sliders[index].classList.add('slider__item_active');
         updateSlider();
     };
 });
 
 // Обновление слайдера
-updateSlider();
+updateSlider(); 
