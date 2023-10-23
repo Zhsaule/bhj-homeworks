5a9acb0096b933abf1d8fce49da6c5473b54201f
zhsaule <zhsaule@gmail.com>
2023-10-24 04:41:30 +0600

    Fix HW9 interest

--- hw9_html-forms/interests/task.js
+++ hw9_html-forms/interests/task.js
@@ -1,21 +1,19 @@
 // Получаем все элементы списка
-const nodeArray = (selector, parent=document) => [].slice.call(parent.querySelectorAll(selector));
-const allThings = nodeArray('input');
-
+const checkboxs = Array.from(document.querySelectorAll('.interest__check'));
 //  global listener
-allThings.forEach((el, index) => {
-    el.addEventListener('click', e => {
+checkboxs.forEach((checkbox, index) => {
+    checkbox.addEventListener('click', e => {
         let check = e.target;
 
         // check/unchek по дочерним включая сам элемент
-        const children = nodeArray('input', check.closest(['label']).parentNode);
+        const children = Array.from((check.closest(['label']).parentNode).querySelectorAll('.interest__check'));
         children.forEach(child => child.checked = check.checked);
-        // проставление родительских элементов
 
+        // проставление родительских элементов
         while(check){
             // поиск родительских элементов и их другие ветки
             const parent = (check.closest(['ul']).parentNode).querySelector('input');
-            const siblings = nodeArray('input', parent.closest('li').querySelector(['ul']));
+            const siblings = Array.from((parent.closest('li').querySelector(['ul'])).querySelectorAll('.interest__check'));
 
             // подсчет check/unchek по дочерним
             const checkStatus = siblings.map(check => check.checked);
