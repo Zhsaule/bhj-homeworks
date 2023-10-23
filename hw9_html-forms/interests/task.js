d9f4ef9f5cd590b1f784c2600b7eaced0d1ac7ae
zhsaule <zhsaule@gmail.com>
2023-10-24 03:51:33 +0600

    Fix HW9 checkbox

--- hw9_html-forms/interests/task.js
+++ hw9_html-forms/interests/task.js
@@ -1,16 +1,33 @@
 // Получаем все элементы списка
-const mainListItems = document.querySelectorAll('.interest__check');
+const nodeArray = (selector, parent=document) => [].slice.call(parent.querySelectorAll(selector));
+const allThings = nodeArray('input');
 
-// Добавляем обработчик события для каждого элемента списка
-mainListItems.forEach((item) => {
-  item.addEventListener('click', () => {
-    // Получаем все вложенные списки для текущего элемента
-    const nestedLists = Array.from(item.closest('.interest').querySelectorAll('.interest__check'));
-    console.log(nestedLists);
-    // Проходимся по каждому вложенному списку
-    nestedLists.forEach((nestedList) => {
-      console.log(nestedList);
-      nestedList.checked = item.checked;
+//  global listener
+allThings.forEach((el, index) => {
+    el.addEventListener('click', e => {
+        let check = e.target;
+
+        // check/unchek по дочерним включая сам элемент
+        const children = nodeArray('input', check.closest(['label']).parentNode);
+        children.forEach(child => child.checked = check.checked);
+        // проставление родительских элементов
+
+        while(check){
+            // поиск родительских элементов и их другие ветки
+            const parent = (check.closest(['ul']).parentNode).querySelector('input');
+            const siblings = nodeArray('input', parent.closest('li').querySelector(['ul']));
+
+            // подсчет check/unchek по дочерним
+            const checkStatus = siblings.map(check => check.checked);
+            const every = checkStatus.every(Boolean);
+            const some = checkStatus.some(Boolean);   
+
+            // выставление состояния checkbox
+            parent.checked = every;   
+            parent.indeterminate = !every && every !== some;
+
+            // переход вверх по дереву
+            check = check != parent ? parent : 0;
+        }
     });
-  });
 });
